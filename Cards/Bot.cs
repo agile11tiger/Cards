7dc83da1b63f3d45dd58aa6b5d5bf038637ed249
KaEvDm <1tyz1@mail.ru>
2019-03-27 10:04:42 -0700

    Revert "well known algorithms"
    This reverts commit 602790726886419c8a784d2c6e3d79c9b721f31c.

--- Cards/Bot.cs
+++ Cards/Bot.cs
@@ -70,5 +70,31 @@ namespace Cards
             if (Game.IsTaker == true) Thread.Sleep(11000);
             return player.Hand.Count + 2;
         }
+        
+        private void BubbleSort(Player player, List<Card> list)
+        {
+            var listTrump = new List<Card>();
+            for (int i = 0; i < list.Count; i++)
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    var card = list[j + 1];
+                    if (CardsComparer.IsTrump(card) && counter != 1)
+                    {
+                        listTrump.Add(card);
+                        list.Remove(card);
+                        j--;
+                        continue;
+                    }
+                    else if (list[j].Value > list[j + 1].Value)
+                    {
+                        list[j + 1] = list[j];
+                        list[j] = card;
+                    }
+                }
+
+            counter++;
+            if (counter == 1) BubbleSort(player, listTrump);
+            if (counter != 3) { player.Hand.AddRange(list); counter++; }
+        }
     }
 }
